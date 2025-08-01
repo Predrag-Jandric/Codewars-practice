1fbc1fc3f0e382ca5a2d725a96b3129a2094ec2b
predrag jandric <predrag.jandric.bg@gmail.com>
2025-08-01 22:42:34 +0200

    will: finish the basic features of a game and do refactoring

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
-import { useState } from "react";
+import Board from "./Board";
 
-function Square({ value, onSquareClick }) {
+export default function Game() {
   return (
-    <button onClick={onSquareClick} className="square">
-      {value}
-    </button>
-  );
-}
-
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
-  function handleClick(i) {
-    const nextSquares = squares.slice();
-    nextSquares[i] = "X";
-    setSquares(nextSquares);
-  }
-
-  return (
-    <section>
-      <div className="board-row">
-        <Square onSquareClick={() => handleClick(0)} value={squares[0]} />
-        <Square onSquareClick={() => handleClick(1)} value={squares[1]} />
-        <Square onSquareClick={() => handleClick(2)} value={squares[2]} />
+    <div className="game">
+      <div className="game-board">
+        <Board />
       </div>
-      <div className="board-row">
-        <Square onSquareClick={() => handleClick(3)} value={squares[3]} />
-        <Square onSquareClick={() => handleClick(4)} value={squares[4]} />
-        <Square onSquareClick={() => handleClick(5)} value={squares[5]} />
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
       </div>
-      <div className="board-row">
-        <Square onSquareClick={() => handleClick(6)} value={squares[6]} />
-        <Square onSquareClick={() => handleClick(7)} value={squares[7]} />
-        <Square onSquareClick={() => handleClick(8)} value={squares[8]} />
-      </div>
-    </section>
+    </div>
   );
 }
